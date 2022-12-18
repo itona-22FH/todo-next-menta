52e94a671c118b09c6c054886ab228f1a4963eca
itona <naov7700@gmail.com>
2022-12-18 20:39:52 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,37 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { v4 as uuidv4 } from "uuid";
+import Link from "next/link";
+import { SetStateAction, useEffect, useState } from "react";
 
 export default function Home() {
+  const [todo, setTodo] = useState("");
+  const [sortedTodo, setSortedTodo] = useState<todosType[]>([]);
+
+  type todosType = {
+    inputText: string;
+    id: string;
+    checked: boolean;
+    edit: boolean;
+  };
+
+  const handleInputTodo = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setTodo(e.target.value);
+  };
+
+  const handleFormSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setSortedTodo([
+      { id: uuidv4(), inputText: todo.trim(), edit: false, checked: false },
+      ...sortedTodo,
+    ]);
+    setTodo("");
+    console.log(sortedTodo);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,60 +41,32 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1>TODOリスト</h1>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            value={todo}
+            name="todo"
+            onChange={handleInputTodo}
+          ></input>
+          <button type="submit">タスク追加</button>
+        </form>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <ul>
+          {sortedTodo.map((todo) => {
+            return <li key={todo.id}>{todo.inputText}</li>;
+          })}
+        </ul>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <button>
+            <Link href="/completeTodo">完了したタスク一覧</Link>
+          </button>
+          <button>
+            <Link href="/notCompleteTodo">未完了のタスク一覧</Link>
+          </button>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
