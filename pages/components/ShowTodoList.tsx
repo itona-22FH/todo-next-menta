d1902391c5804268ab88c692f4ab89cf2511c7f6
itona <naov7700@gmail.com>
2023-01-01 21:37:03 +0900

    Propsの型の追加、再投稿ボタンと入力欄の追加

--- pages/components/ShowTodoList.tsx
+++ pages/components/ShowTodoList.tsx
@@ -1,49 +1,76 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { TodoActionButton } from "./TodoActionButton";
 
-export const ShowTodoList = (props: {
-  todoArray: any;
-  handleDeleteTodo: any;
-  handleOnCheck: any;
-  setEditId: any;
-}) => {
-  const { todoArray, handleDeleteTodo, handleOnCheck, setEditId } = props;
+type Todo = {
+  inputText: string;
+  id: string;
+  checked: boolean;
+  edit: boolean;
+};
+
+type Props = {
+  todoArray: Todo[];
+  handleDeleteTodo: (id: string) => void;
+  handleCheckTodo: (id: string, checked: boolean) => void;
+  handleEditTodo: (id: string) => void;
+  editTodoId: string;
+  editTodoText: string;
+  setEditTodoText: Dispatch<SetStateAction<string>>;
+  setEditTodoId: Dispatch<SetStateAction<string>>;
+};
+
+export const ShowTodoList = ({
+  todoArray,
+  handleDeleteTodo,
+  handleCheckTodo,
+  handleEditTodo,
+  editTodoId,
+  editTodoText,
+  setEditTodoId,
+  setEditTodoText,
+}: Props) => {
   return (
     <>
-      {todoArray.map(
-        (todo: {
-          id: React.Key | null | undefined;
-          inputText:
-            | string
-            | number
-            | boolean
-            | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-            | React.ReactFragment
-            | React.ReactPortal
-            | null
-            | undefined;
-          checked: boolean | undefined;
-        }) => {
-          return (
-            <li key={todo.id}>
-              {todo.inputText}
+      {todoArray.map((todo) => (
+        <li key={todo.id}>
+          {todo.inputText}
+          {todo.id === editTodoId ? (
+            <>
+              <input
+                type="text"
+                placeholder="編集内容を入力"
+                value={editTodoText}
+                onChange={(e) => setEditTodoText(e.target.value)}
+              />
+              <TodoActionButton
+                id={todo.id}
+                handleOnClick={handleEditTodo}
+                text="再投稿"
+              />
+            </>
+          ) : (
+            <>
               <TodoActionButton
                 id={todo.id}
                 text="削除"
-                handle={handleDeleteTodo}
+                handleOnClick={handleDeleteTodo}
               />
-              <TodoActionButton id={todo.id} text="編集" handle={setEditId} />
+              <TodoActionButton
+                id={todo.id}
+                text="編集"
+                handleOnClick={setEditTodoId}
+              />
               <input
                 type="checkbox"
                 checked={todo.checked}
                 onChange={() => {
-                  handleOnCheck(todo.id, todo.checked);
+                  handleCheckTodo(todo.id, todo.checked);
                 }}
               ></input>
-            </li>
-          );
-        }
-      )}
+            </>
+          )}
+        </li>
+      ))}
     </>
   );
 };
