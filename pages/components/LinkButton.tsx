efa6bcdc0210e54b4a7b5556161ad1b143e276a6
itona <naov7700@gmail.com>
2023-01-01 19:19:35 +0900

    Changed to destructuring props

--- pages/components/LinkButton.tsx
+++ pages/components/LinkButton.tsx
@@ -1,11 +1,12 @@
-import Link from 'next/link'
-import React from 'react'
-import { UrlObject } from 'url'
+import Link from "next/link";
+import React from "react";
+import { UrlObject } from "url";
 
-export const LinkButton = (props: { url: string | UrlObject, text: string}) => {
+export const LinkButton = (props: { url: string; text: string }) => {
+  const { url, text } = props;
   return (
     <button>
-    <Link href={props.url}>{props.text}</Link>
+      <Link href={url}>{text}</Link>
     </button>
-  )
-}
+  );
+};
