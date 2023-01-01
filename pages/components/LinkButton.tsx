ec5e6535de714b3fafb178f27f50dcbd0d9227e4
itona <naov7700@gmail.com>
2023-01-01 21:36:00 +0900

    Propsの型の追加

--- pages/components/LinkButton.tsx
+++ pages/components/LinkButton.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import React from "react";
-import { UrlObject } from "url";
 
-export const LinkButton = (props: { url: string; text: string }) => {
-  const { url, text } = props;
+type Props = {
+  url: string;
+  text: string;
+};
+export const LinkButton = ({ url, text }: Props) => {
   return (
     <button>
       <Link href={url}>{text}</Link>
