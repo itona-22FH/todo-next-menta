2a320f0b0035bf57828b6cf0a9917fac5d1b5b07
itona <naov7700@gmail.com>
2023-01-01 19:15:56 +0900

    Add type declaration to props

--- pages/components/TodoForm.tsx
+++ pages/components/TodoForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const TodoForm = (props) => {
+export const TodoForm = (props: { todo: any; handleFormSubmit: any; handleInputTodo: any }) => {
     const {todo, handleFormSubmit, handleInputTodo} = props
   return (
     <form onSubmit={handleFormSubmit}>
