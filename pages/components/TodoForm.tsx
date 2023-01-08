b45bb7b806a5d3e0b8d3fc4647245b21a220f414
itona <naov7700@gmail.com>
2023-01-08 15:05:31 +0900

    Adding functions and removing properties

--- pages/components/TodoForm.tsx
+++ pages/components/TodoForm.tsx
@@ -1,14 +1,27 @@
 import React, { SetStateAction } from "react";
+import { useRecoilState } from "recoil";
+import { todoListState, todoState } from "./store/Auth/auth";
+import { v4 as uuidv4 } from "uuid";
 
+export const TodoForm = ({ handleButtonDisabled }: FormProps) => {
+  const [todos, setTodos] = useRecoilState(todoListState);
+  const [todo, setTodo] = useRecoilState(todoState);
 
+  const handleInputTodo = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setTodo(e.target.value);
+  };
 
-export const TodoForm = ({
-todo,
-  todoArray,
-  handleFormSubmit,
-  handleInputTodo,
-  handleDisabled,
-}: FormProps) => {
+  const handleFormSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setTodos([
+      { id: uuidv4(), inputText: todo.trim(), edit: false, checked: false },
+      ...todos,
+    ]);
+    setTodo("");
+    console.log(todos);
+  };
   return (
     <form onSubmit={handleFormSubmit}>
       <input
@@ -16,10 +29,11 @@ todo,
         value={todo}
         name="todo"
         onChange={handleInputTodo}
-        disabled={handleDisabled(todoArray)}
+        disabled={handleButtonDisabled(todos)}
       ></input>
-      <button type="submit"
-      disabled={handleDisabled(todoArray)}>タスク追加</button>
+      <button type="submit" disabled={handleButtonDisabled(todos)}>
+        タスク追加
+      </button>
     </form>
   );
 };
