f36d02960acc2ebea9ceec2d112a7a33ef1636f1
itona <naov7700@gmail.com>
2023-01-08 15:03:21 +0900

    Add useRecoilValue

--- pages/components/LinkButton.tsx
+++ pages/components/LinkButton.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { todoListState } from "./store/Auth/auth";
 
-export const LinkButton = ({ url, text, todoArray, handleDisabled }: LinkButtonProps) => {
+export const LinkButton = ({ url, text, handleDisabled }: LinkButtonProps) => {
+  const todos = useRecoilValue(todoListState);
   return (
-    <button disabled={handleDisabled(todoArray)}>
+    <button disabled={handleDisabled(todos)}>
       <Link href={url}>{text}</Link>
     </button>
   );
