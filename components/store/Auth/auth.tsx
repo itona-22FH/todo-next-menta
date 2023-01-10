b9dfdc88b4adf2dd0fb377f73a6f2636d989d977
itona <naov7700@gmail.com>
2023-01-10 23:55:04 +0900

    Add State for Sort function

--- components/store/Auth/auth.tsx
+++ components/store/Auth/auth.tsx
@@ -9,7 +9,18 @@ export const todoState = atom<string>({
   key: "todoState",
   default: "",
 });
+
 export const todoEditState = atom<string>({
   key: "todoEditState",
   default: "",
+});
+
+export const sortedTodoState = atom<Todo[]>({
+  key: "sortedTodoState",
+  default: [],
+});
+
+export const sortKeyState = atom<string>({
+  key: "sortKeyState",
+  default: "all",
 });
