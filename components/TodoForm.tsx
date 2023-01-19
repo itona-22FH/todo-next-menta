dd0881fc3588aaa06ed2f729d4607760edc14676
itona <naov7700@gmail.com>
2023-01-19 19:12:09 +0900

    Adding tasks to Firebase implement the function

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -5,6 +5,8 @@ import { todoState } from "./store/atoms/todoState";
 import { v4 as uuidv4 } from "uuid";
 import { Box, Input } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import db from "../firebase/firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
 
 export const TodoForm = ({ handleButtonDisabled }: FormProps) => {
   const [todos, setTodos] = useRecoilState(todoListState);
@@ -18,6 +20,18 @@ export const TodoForm = ({ handleButtonDisabled }: FormProps) => {
 
   const handleFormSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
+    (async () => {
+      try {
+        const docRef = await addDoc(collection(db, "next-todo-menta"), {
+          text: todo,
+          checked: false,
+          edit: false,
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    })();
     setTodos([
       { id: uuidv4(), inputText: todo.trim(), edit: false, checked: false },
       ...todos,
